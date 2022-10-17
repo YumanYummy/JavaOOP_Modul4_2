a49604c39573a61f20c7c1d7d9ecb64577cee8cb
YumanYummy <ahmadrizki0209@gmail.com>
2022-10-17 22:16:21 +0700

    Modul4_2: Edit Specifications

--- src/Devices/Drivers.java
+++ src/Devices/Drivers.java
@@ -3,7 +3,7 @@ package Devices;
 public class Drivers {
     public static void main(String[] args) {
         Smartphone s = new Smartphone("Apple Iphone 11","Apple A13 Bionic Procesor");
-        Specs spek = new Specs("Apple A13 Bionic","12Mp Camera","12 MP Selfie Camera",s);
+        Specs spek = new Specs("Apple A13 Bionic","1850MAh Battery","12 MP Selfie Camera",s);
         System.out.println("Smartphone: " +spek.getSmartphone().getSmartphoneNamee());
         System.out.println("Specification 1 : " +spek.getSpecs1().getSpecss1());
         System.out.println("Specification 2 : " +spek.getSpecs2());
