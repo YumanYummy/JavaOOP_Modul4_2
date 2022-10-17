6277353f6fb70d2c51b2dcea8e6e85d9c89802a2
YumanYummy <ahmadrizki0209@gmail.com>
2022-10-17 22:03:05 +0700

    Modul4_2 : Edit Method Initialization

--- src/Devices/Drivers.java
+++ src/Devices/Drivers.java
@@ -3,7 +3,7 @@ package Devices;
 public class Drivers {
     public static void main(String[] args) {
         Smartphone s = new Smartphone("Apple Iphone 11","Apple A13 Bionic Procesor");
-        Specs spek = new Specs("Apple A13 Bionic","12Mp Camera","Selfie",s);
+        Specs spek = new Specs("Apple A13 Bionic","12Mp Camera","Selfie Camera",s);
         System.out.println("Smartphone: " +spek.getSmartphone().getSmartphoneNamee());
         System.out.println("Specs 1 : " +spek.getSpecs1().getSpecss1());
         System.out.println("Specs 2 : " +spek.getSpecs2());
